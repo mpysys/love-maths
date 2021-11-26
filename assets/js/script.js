b69f509be3b63bfe259bb19f831a1d31d94b4983
mpysys <monnet.morgan@gmail.com>
2021-11-26 16:38:55 +0000

    Add favicon, and final tidy ups for the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,10 +11,15 @@ document.addEventListener("DOMContentLoaded", function(){
             } else {
                 let gameType = this.getAttribute("data-type");
                 runGame(gameType);
-                alert(`You clicked ${gameType}`)
             }
         })
     }
+
+    document.getElementById('answer-box').addEventListener('keydown', function(event) {
+        if(event.key === "Enter") {
+            checkAnswer();
+        }
+    });
 
     runGame("addition");
 });
@@ -24,6 +29,10 @@ document.addEventListener("DOMContentLoaded", function(){
  * and after the user's answer has been processed  
  */
 function runGame(gameType){
+
+    document.getElementById('answer-box').value = "";
+    document.getElementById('answer-box').focus();
+
     // creates two random numbers between 1 and 25
     let num1 = Math.floor(Math.random()*25) +1;
     let num2 = Math.floor(Math.random()*25) +1;
